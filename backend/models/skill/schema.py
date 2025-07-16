1d37f866144db644b073ec1b498c5480abaf088c
Muhammad Faizan Shakir <mr.shakir117@live.com>
2025-07-16 21:07:26 +0500

    Backend Architecture & ORM Enhancements

--- backend/models/skill/schema.py
+++ backend/models/skill/schema.py
@@ -3,20 +3,14 @@ from .base import allowed_levels
 
 class SkillIn(BaseModel):
     name:str = Field(...,min_length=2,max_length=50)
-    level:str = Field(...,min_length=2)
+    skill_level_id:int
 
-    @validator('name','level',pre=True)
+    @validator('name','skill_level_id',pre=True)
     def trim_fields(cls,v):
         if not (isinstance(v,str)):
             return v
         return v.strip()
-
-    @validator('level')
-    def validate_level(cls,v):
-         valid_levels = allowed_levels()
-         if(v not in valid_levels):
-             raise ValueError(f"Level must be one of {', '.join(valid_levels)}")
-         return v
     
-class Skill(SkillIn):
+class SkillRead(SkillIn):
     id:int
+    level:str
---
+++ backend/models/skill_level/base.py
@@ -0,0 +1,4 @@
+from typing import Set
+
+def allowed_levels() -> Set[str]:
+    return {"Beginner", "Intermediate", "Advanced"}
