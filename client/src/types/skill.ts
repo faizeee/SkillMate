367e0c637b5d462a63e8fe88d25a75807b643303
Muhammad Faizan Shakir <mr.shakir117@live.com>
2025-09-02 16:07:49 +0500

    Day 20 - frontent client side UI imporved with better layout and placement for skill icon in skills list

--- client/src/types/skill.ts
+++ client/src/types/skill.ts
@@ -3,10 +3,15 @@ export type NewSkill = {
   skill_level_id: string | number;
 };
 // Skill type definition
-export type Skill = { id: number; level_name: string } & NewSkill; //Composition type or we can directly add name and level attr in Skill
+export type Skill = NewSkill & {
+  id: number;
+  level_name: string;
+  icon_url?:string;
+  icon_path?:string;
+}; //Composition type or we can directly add name and level attr in Skill
 
 export const SKILL_LEVELS = {
-    BEGINNER: 1,
-    INTERMEDIATE: 2,
-    EXPERT: 3,
+  BEGINNER: 1,
+  INTERMEDIATE: 2,
+  EXPERT: 3,
 } as const;
